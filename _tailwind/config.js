1d80903fa4b6dc474e71968b0683df764fb2a221
Wender Alves <wender.sobrinho@elitesoft.com.br>
2022-08-08 08:56:44 -0300

    :feat Criação do layout com tailwind

---
+++ _tailwind/config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                primary: '#003475',
+                primary__hover: '#002055',
+                secundary: '#00b3bc',
+                info: '#6AB2FF',
+                success: '#ADE46C',
+                warning: '#FFC666',
+                danger: '#FF405D',
+            },
+            backgroundImage: {
+                'hero-pattern': "url('/assets/images/fundo-abstrato-azul.jpg')",
+            }
+        }
+    }
+};
